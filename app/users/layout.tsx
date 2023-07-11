143830664a196a007b5db1cb2c010ca68207c250
Willians Jimenez <williansaaid@hotmail.com>
2023-07-10 20:13:30 -0500

    users fix

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -10,7 +10,6 @@ export default async function UsersLayout({
     const users = await getUsers();
 
     return (
-        //@ts-expect-error Server component
         <Sidebar>
             <div
             className="h-full"
